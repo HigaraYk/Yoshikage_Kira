d2a0e8d21e02e428d41ab9925aa2f0e3685a1bf9
Higara <Higarayk@gmail.com>
2026-05-18 10:41:19 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+
+#Comentário unico
+
+'''
+Comentário 
+longo
+'''
+
+
+print("Vamos começar")
+print("-")
+print("-")
+print("-")
+
+
+nome = input('Qual é seu nome?')
+
+idade = int(input('Qual é sua idade?'))
+
+if idade <18: print('idade insuficiente para continuar')
+else:
+    Genero = input('Qual é o seu genero?')
+
+
+
+
+
+
+
+
