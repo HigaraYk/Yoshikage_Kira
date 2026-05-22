526ec9ed4cb5030f8f6952e9efd45a2e9d5807c5
Higara <Higarayk@gmail.com>
2026-05-22 09:48:56 -0300

    Update and rename main.py to nivel1.py

---
+++ nivel1.py
@@ -0,0 +1,51 @@
+
+
+
+#Yoshikage Kira
+
+''' 
+O meu nome é Yoshikage Kira. Tenho 33 anos de idade, moro na região noroeste na 
+cidade de Morioh, onde ficam as casas de veraneio, eu não sou casado. 
+Eu sou executivo da loja de departamentos Kame Yu e chego em casa todas as 
+noites por volta das 8 horas. Eu não sou fumante e bebo apenas socialmente. 
+Eu me deito sempre as 11 horas da noite e faço questão de dormir 8 horas, 
+não importa o que aconteça. Desde que eu possa tomar um copo de leite 
+quente e me exercitar fazendo alongamentos por pelo menos 20 minutos antes de ir
+para cama, eu costumo ter um sono ininterrupto. Eu acordo e me levando pela 
+manhã sem o mínimo estresse ou cansaço, como se fosse um bebezinho 
+despreocupado. No meu último check-up de saúde, não encontraram nenhum problema. 
+
+Eu só estou dizendo tudo isso para tentar explicar que sou uma pessoa que 
+procura manter a paz interior o tempo todo. Eu sempre evito problemas ou criar
+inimizades, e também evito me preocupar com vitórias ou derrotas que me façam 
+perder o sono à noite. Essa é minha filosofia para lidar com a sociedade, 
+e eu tenho certeza de que, agindo 'assim, eu consigo ser feliz.
+'''
+
+
+
+print("Vamos começar")
+
+nome = input('Qual é seu nome?')
+
+idade = int(input('Qual é sua idade?'))
+
+if idade <18:
+    print('idade insuficiente para continuar')
+
+else:
+        regiao = input('Qual região você mora?')
+        
+        cidade = input('Qual cidade você mora?')
+        
+        empresa = input('Qual empresa você trabalha?')
+        
+        funcao = input('Você é funcionário ou executivo?')
+        
+        idade_final = str(idade)
+
+
+
+print ('O meu nome é ' + nome + '.' +' Tenho ' + idade_final + ' anos de idade, moro na região ' + regiao + ' na cidade de ' + cidade + ', onde ficam as casas de veraneio, eu não sou casado. Eu sou ' + funcao + ' da loja de departamentos ' + empresa + ' e chego em casa todas as noites por volta das 8 horas. Eu não sou fumante e bebo apenas socialmente. Eu me deito sempre as 11 horas da noite e faço questão de dormir 8 horas, não importa o que aconteça. Desde que eu possa tomar um copo de leite quente e me exercitar fazendo alongamentos por pelo menos 20 minutos antes de ir para cama, eu costumo ter um sono ininterrupto. Eu acordo e me levando pela manhã sem o mínimo estresse ou cansaço, como se fosse um bebezinho despreocupado. No meu último check-up de saúde, não encontraram nenhum problema. Eu só estou dizendo tudo isso para tentar explicar que sou uma pessoa que procura manter a paz interior o tempo todo. Eu sempre evito problemas ou criar inimizades, e também evito me preocupar com vitórias ou derrotas que me façam perder o sono à noite. Essa é minha filosofia para lidar com a sociedade, e eu tenho certeza de que, agindo assim, eu consigo ser feliz.')
+
+
